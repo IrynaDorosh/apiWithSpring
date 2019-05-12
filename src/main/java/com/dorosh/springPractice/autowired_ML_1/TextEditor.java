54729268befc761c653e095d694791e8967d1239
Iryna Dorosh <iryna.karakata@gmail.com>
2019-05-12 14:20:01 +0300

    autowired on property

--- src/main/java/com/dorosh/springPractice/autowired_ML_1/TextEditor.java
+++ src/main/java/com/dorosh/springPractice/autowired_ML_1/TextEditor.java
@@ -2,16 +2,14 @@ package com.dorosh.springPractice.autowired_ML_1;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TextEditor {
+
+    @Autowired
     private SpellChecker spellChecker;
 
     public TextEditor() {
         System.out.println("inside TE constructor");
     }
 
-    @Autowired
-    public void setSpellChecker( SpellChecker spellChecker ){
-        this.spellChecker = spellChecker;
-    }
     public SpellChecker getSpellChecker( ) {
         return spellChecker;
     }
