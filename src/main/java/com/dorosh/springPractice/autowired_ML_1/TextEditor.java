cb6b1f8f437c270c59abf5789b0fbbe5d92c64c0
Iryna Dorosh <iryna.karakata@gmail.com>
2019-05-12 14:27:00 +0300

    autowired on constructor

--- src/main/java/com/dorosh/springPractice/autowired_ML_1/TextEditor.java
+++ src/main/java/com/dorosh/springPractice/autowired_ML_1/TextEditor.java
@@ -1,18 +1,21 @@
 package com.dorosh.springPractice.autowired_ML_1;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TextEditor {
 
-    @Autowired
+
     private SpellChecker spellChecker;
 
-    public TextEditor() {
-        System.out.println("inside TE constructor");
+    @Autowired
+    public TextEditor(SpellChecker spellChecker) {
+        this.spellChecker = spellChecker;
     }
 
-    public SpellChecker getSpellChecker( ) {
+    public SpellChecker getSpellChecker() {
         return spellChecker;
     }
+
     public void spellCheck() {
         spellChecker.checkSpelling();
     }
